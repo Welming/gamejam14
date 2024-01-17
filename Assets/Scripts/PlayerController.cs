4d95b8148819243ebebde7234373571f9b900164
David <ciseauquicoupe@hotmail.com>
2024-01-17 00:39:59 -0500

    Walls shadows animations ++
    Worked on shadow casting and animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -7,31 +8,66 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputActionMap playerControls;
-    public Rigidbody2D player;
     [Range(1.0f, 8.0f)]
     public float moveSpeed = 3;
-    private Vector2 moveDirection = Vector2.zero;
 
-    private void Awake()
+    private Vector2 moveDirection = Vector2.zero;
+    private Vector2 moveMemory = Vector2.zero;
+    Animator animator;
+    Rigidbody2D rigidBody;
+
+    private void ResetAnimatorParameters(string parameter)
     {
-        
+        for (int e = 0; e < animator.parameters.Length;e++)
+        {
+            if(animator.parameters[e].name != "X" && animator.parameters[e].name != "Y")
+            {
+                if(animator.parameters[e].name == parameter)
+                {
+                    animator.SetBool(animator.parameters[e].name, true);
+                }
+
+                if (animator.parameters[e].name != parameter)
+                {
+                    animator.SetBool(animator.parameters[e].name, false);
+                }
+            }
+        }
     }
 
     public void MoveCharacter(InputAction.CallbackContext context)
     {
-        if(context.performed)
+        if (context.performed)
         {
             moveDirection = context.ReadValue<Vector2>();
-            player.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            if(context.ReadValue<Vector2>().y != 0)
+            {
+                moveMemory.y = context.ReadValue<Vector2>().y;
+            }
+            if (context.ReadValue<Vector2>().x != 0)
+            {
+                moveMemory.x = context.ReadValue<Vector2>().x;
+            }
+            rigidBody.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            ResetAnimatorParameters("Walk");
+            animator.SetFloat("X", moveMemory.x);
+            animator.SetFloat("Y", moveMemory.y);
         }
         if (context.canceled)
         {
-            moveDirection = Vector2.zero;
+            ResetAnimatorParameters("Idle");
+            moveDirection = new Vector2(0,0);
         }
+    }
+
+    private void Start()
+    {
+        animator = gameObject.transform.Find("Model").GetComponent<Animator>().GetComponent<Animator>();
+        rigidBody = gameObject.transform.Find("Model").GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        player.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        rigidBody.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 }
