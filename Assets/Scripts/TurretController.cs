6da9c96bdf259f44a17a0af9847d96453dc71d18
xalbec <xalbec1@gmail.com>
2024-01-20 15:11:06 -0500

    Default Turret Prefab ++
    Default turret created. It can detect objects that enter into its area of effect and leave the area.

---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> enemyList;
+    private CircleCollider2D targetArea;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        targetArea = GetComponent<CircleCollider2D>();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D newGuy)
+    {
+        Debug.Log("Hello");
+        enemyList.Add(newGuy.gameObject.transform.parent.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D newGuy)
+    {
+        Debug.Log("Goodbye");
+        enemyList.Remove(newGuy.gameObject.transform.parent.gameObject);
+    }
+
+}
